99c90ffeed4ff5c398ee24c0462c2a5f6d8476aa
Akrem Bc <akrembencheikh@gmail.com>
2021-02-25 17:36:43 +0100

    edited from eclipse

--- src/MyPackage/GitMainClass.java
+++ src/MyPackage/GitMainClass.java
@@ -5,6 +5,7 @@ public class GitMainClass {
 	public static void main(String[] args) {
 		System.out.println("Hello Git");
 		System.out.println("Modified from Github");
+		System.out.println("Modified from Eclipse");
 	}
 
 }
